805819b828b4b73f8ebf9e01a871306bd04fdfdf
carlozrodri <patica100@gmail.com>
2024-11-14 16:39:26 +0000

    Update README with installation and setup instructions, Add docs for views

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# zero_deposit
+# Letting Agents CRM API
+
+This project is a RESTful API designed to serve as the backend for a Customer Relationship Management (CRM) system specifically tailored for letting agents. The API will handle the management of property listings and tenant details.
+
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Setup](#setup)
+- [Usage](#usage)
+- [Testing](#testing)
+
+## Installation
+
+To install the project and set up your environment, follow these steps:
+
+1. Clone the repository:
+
+   git clone https://github.com/carlozrodri/zero_deposit.git
+
+2. Change to the project directory:
+
+    cd zero_deposit
+
+3. Create a virtual enviroment
+
+    python3 -m venv env
+
+4. Activate the virtual enviroment
+Mac/Linux
+    source env/bin/activate
+
+5. Install the project dependencies
+    
+    pip install -r requirements.txt
+
+## Setup
+
+1.Set up the environment variables:
+Create a .env file and add the necessary environment variables:
+
+    SECRET_KEY=your_secret_key
+
+2. Run migrations 
+
+    python manage.py migrate
+
+
+## Usage
+
+To run the development server
+    
+    python manage.py runserver
+
+## Testing
+
+To Run tests
+ python manage.py test
--- properties/views.py
+++ properties/views.py
@@ -1,23 +1,61 @@
-# views.py
 from rest_framework import viewsets, permissions
 from .models import Property
 from .serializers import PropertySerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import PermissionDenied
-from rest_framework.response import Response
-from rest_framework import status
+
+
 
 class PropertyViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for managing Property instances. Only authenticated users 
+    can access this viewset, and users can only view, update, or delete 
+    properties that they created.
+
+    Attributes:
+        queryset: The base queryset to retrieve all Property objects.
+        serializer_class: The serializer used for Property objects.
+        permission_classes: Ensures that only authenticated users 
+                            can access this viewset.
+    """
     queryset = Property.objects.all()
     serializer_class = PropertySerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
+        """
+        Overrides the default queryset to filter properties by the 
+        currently authenticated user, showing only properties created 
+        by them.
+        
+        Returns:
+            Queryset of Property objects created by the current user.
+        """
+
         return Property.objects.filter(created_by=self.request.user)
     def perform_create(self, serializer):
+        """
+        Custom create method to set the 'created_by' field to the 
+        current user when a new property is created.
+
+        Args:
+            serializer: The serializer instance that will save the data.
+        """
         serializer.save(created_by=self.request.user)
     def perform_update(self, serializer):
-        # Ensure the user can only update properties they've created
+        """
+        Custom update method to restrict updates to properties that 
+        the current user has created. Raises a PermissionDenied 
+        exception if the user attempts to update a property they 
+        didn't create.
+
+        Args:
+            serializer: The serializer instance that will save the data.
+        
+        Raises:
+            PermissionDenied: If the current user is not the creator of 
+                              the property being updated.
+        """
         if serializer.instance.created_by != self.request.user:
             raise PermissionDenied("You do not have permission to update this property.")
         super().perform_update(serializer)
