df9f94cc94c6208747d9c81a4ffaf684c1c86317
carlozrodri <patica100@gmail.com>
2024-11-14 15:21:54 +0000

    Add User model with custom fields

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    is_admin = models.BooleanField(default=False)
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name="custom_user_set",  # Change this to avoid conflicts
+        blank=True,
+        help_text="The groups this user belongs to.",
+        verbose_name="groups"
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="custom_user_permissions",  # Change this to avoid conflicts
+        blank=True,
+        help_text="Specific permissions for this user.",
+        verbose_name="user permissions"
+    )
