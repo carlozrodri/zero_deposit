7f016306b639636b2abd266f0c37d6667880a8ad
carlozrodri <patica100@gmail.com>
2024-11-14 15:24:00 +0000

    Add Property model

--- properties/models.py
+++ properties/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Property(models.Model):
+    address = models.CharField(max_length=255)
+    postcode = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    rooms = models.IntegerField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.address
